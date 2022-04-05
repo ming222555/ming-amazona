0d465982fe437515d3d0d584b42f894cbba9a3c7
ming222555 <ming222555@yahoo.com>
2022-04-05 21:54:01 +0800

    Edit package.json * change "next lint" to "next lint -d src" * Move pages/ directory to under src/. Running "yarn lint" will pick up issues in files at src/ as well as its subdirectories. To verify, * create some directory under src, say, ok/ directory. * create a component file inside ok/, say Ua.tsx, that has some lint issues. Run "yarn lint". Lint issues should appear for src/ok/Ua.tsx

---
+++ src/ok/Ua.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from '@mui/material/Link';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
+import Typography from '@mui/material/Typography';
+
+function Ua() {
+  return <div>hello world!</div>;
+}
+
+export default Ua;
