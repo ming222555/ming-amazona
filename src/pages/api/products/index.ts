ef8cec9dac52858d94bae5613502089fbad1dc80
ming222555 <ming222555@yahoo.com>
2022-04-11 12:01:06 +0800

    Next Routes api ... * yarn add next-connect * seed mongo * GET request all products

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import Product from '../../../db/models/Product';
+import db from '../../../db/db';
+
+// https://stackoverflow.com/questions/67009540/property-status-does-not-exist-on-type-serverresponse-ts2339
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const products = await Product.find({});
+  await db.disconnect();
+  res.send(products);
+});
+
+export default handler;
---
+++ src/pages/api/seed.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import Product from '../../db/models/Product';
+import db from '../../db/db';
+import data from '../../db/data';
+
+// https://stackoverflow.com/questions/67009540/property-status-does-not-exist-on-type-serverresponse-ts2339
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+handler.get(async (req, res) => {
+  await db.connect();
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
+  await db.disconnect();
+  res.send({ message: 'seeded successfully' });
+});
+
+export default handler;
