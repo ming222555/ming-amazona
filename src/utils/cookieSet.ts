face127a88cf51be33bdbfbc13ace73303993c71
ming222555 <ming222555@yahoo.com>
2022-04-13 11:27:44 +0800

    In cookieSet.ts, add console log ... * You're trying to set Cookie in NON BROWSER environment!

--- src/utils/cookieSet.ts
+++ src/utils/cookieSet.ts
@@ -2,6 +2,8 @@ import Cookies from 'js-cookie';
 
 export default function cookieSet(name: string, val: string | number | boolean | object): void {
   if (typeof window === 'undefined') {
+    // eslint-disable-next-line no-console
+    console.log("You're trying to set Cookie in NON BROWSER environment!");
     return;
   }
   const valType = typeof val;
@@ -10,12 +12,12 @@ export default function cookieSet(name: string, val: string | number | boolean |
     Cookies.set(name, val as string);
     return;
   }
-  if (valType === 'number' || valType === 'boolean') {
-    Cookies.set(name, val.toString());
-    return;
-  }
   if (valType === 'object') {
     Cookies.set(name, JSON.stringify(val));
+    return;
+  }
+  if (valType === 'number' || valType === 'boolean') {
+    Cookies.set(name, val.toString());
     return;
   }
 
