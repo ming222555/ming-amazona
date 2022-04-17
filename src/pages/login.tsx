85310c2f9cfea661b56c23e69dfddf81d8594c61
ming222555 <ming222555@yahoo.com>
2022-04-17 23:02:25 +0800

    Login page... * replace form with react-hook-form * replace browser alert with Snackbar * field validation with react-hook-form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Typography from '@mui/material/Typography';
@@ -6,9 +6,11 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
 import { styled } from '@mui/material/styles';
 
 import axios from 'axios';
+import { Controller, useForm } from 'react-hook-form';
 
 import Layout from '../components/Layout';
 import Link from '../components/Link';
@@ -24,11 +26,29 @@ const StyledLoginForm = styled('form')({
   },
 });
 
+interface IFFormData {
+  email: string;
+  password: string;
+}
+
 const LoginPage: NextPage = () => {
+  // https://stackoverflow.com/questions/71275687/type-of-handlesubmit-parameter-in-react-hook-form
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFFormData>();
+
   const router = useRouter();
   const { redirect } = router.query; // login?redirect=/shipping
   const { state, dispatch } = useContext(StateContext);
   const { userInfo } = state;
+
+  const [alert, setAlert] = useState({
+    open: false,
+    message: '',
+    backgroundColor: '',
+  });
 
   const redirectTo: string = (redirect as string) ? (redirect as string) : '/';
 
@@ -38,57 +58,101 @@ const LoginPage: NextPage = () => {
     isLogined = true;
     router.push(redirectTo);
   }
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const submitHandler = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault();
+  const submitHandler = async ({ email, password }: IFFormData): Promise<void> => {
     try {
       const { data } = await axios.post<LoginRes>('/api/users/login', { email, password });
       dispatch({ type: 'USER_LOGIN', payload: data as IFTokenUser });
     } catch (err) {
-      alert(err.response.data ? err.response.data.errormsg : err.message);
+      // alert(err.response.data ? err.response.data.errormsg : err.message);\
+      setAlert({
+        open: true,
+        message: err.response.data ? err.response.data.errormsg : err.message,
+        backgroundColor: '#FF3232',
+      });
     }
   };
 
   return (
     <Layout title="Login">
-      <StyledLoginForm className={`${PREFIX}-loginform`} onSubmit={submitHandler}>
+      <StyledLoginForm className={`${PREFIX}-loginform`} onSubmit={handleSubmit(submitHandler)}>
         <Typography variant="h1">Login</Typography>
         {isLogined ? (
           <Typography>Redirecting to {redirectTo === '/' ? 'home' : redirectTo.substring(1)} page...</Typography>
         ) : (
-          <List>
-            <ListItem>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="email"
-                label="Email"
-                inputProps={{ type: 'email' }}
-                onChange={(e): void => setEmail(e.target.value)}
-              />
-            </ListItem>
-            <ListItem>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="password"
-                label="Password"
-                inputProps={{ type: 'password' }}
-                onChange={(e): void => setPassword(e.target.value)}
-              />
-            </ListItem>
-            <ListItem>
-              <Button variant="contained" color="primary" type="submit" fullWidth>
-                Login
-              </Button>
-            </ListItem>
-            <ListItem>
-              Don&lsquo;t have an account?&nbsp;
-              <Link href={`/register?redirect=${redirectTo}`}>Register</Link>
-            </ListItem>
-          </List>
+          <>
+            <List>
+              <ListItem>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ }}
+                  render={({ field }): JSX.Element => (
+                    <TextField
+                      variant="outlined"
+                      fullWidth
+                      id="email"
+                      label="Email"
+                      inputProps={{ type: 'email' }}
+                      error={Boolean(errors.email)}
+                      helperText={
+                        errors.email
+                          ? errors.email.type === 'pattern'
+                            ? 'Email is not valid'
+                            : 'Email is required'
+                          : ''
+                      }
+                      {...field}
+                    />
+                  )}
+                />
+              </ListItem>
+              <ListItem>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true, minLength: 6 }}
+                  render={({ field }): JSX.Element => (
+                    <TextField
+                      variant="outlined"
+                      fullWidth
+                      id="password"
+                      label="Password"
+                      inputProps={{ type: 'password' }}
+                      error={Boolean(errors.password)}
+                      helperText={
+                        errors.password
+                          ? errors.password.type === 'minLength'
+                            ? 'Password length is more than 5'
+                            : 'Password is required'
+                          : ''
+                      }
+                      {...field}
+                    />
+                  )}
+                />
+              </ListItem>
+              <ListItem>
+                <Button variant="contained" color="primary" type="submit" fullWidth>
+                  Login
+                </Button>
+              </ListItem>
+              <ListItem>
+                Don&lsquo;t have an account?&nbsp;
+                <Link href={`/register?redirect=${redirectTo}`}>Register</Link>
+              </ListItem>
+            </List>
+            <Snackbar
+              open={alert.open}
+              message={alert.message}
+              ContentProps={{ style: { backgroundColor: alert.backgroundColor } }}
+              anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              onClose={(): void => setAlert({ ...alert, open: false })}
+              autoHideDuration={4000}
+            />
+          </>
         )}
       </StyledLoginForm>
     </Layout>
