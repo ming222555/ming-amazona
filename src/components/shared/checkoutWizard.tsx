94be103bc4f2f212c94a81fab314a313c91cc1eb
ming222555 <ming222555@yahoo.com>
2022-04-19 16:39:51 +0800

    PaymentPage... * react-hook-form on <RadioGroup> with validation

--- src/components/shared/checkoutWizard.tsx
+++ src/components/shared/checkoutWizard.tsx
@@ -5,7 +5,7 @@ import Stepper from '@mui/material/Stepper';
 
 export default function CheckoutWizard(props: { activeStep: number }): JSX.Element {
   return (
-    <Stepper activeStep={props.activeStep} alternativeLabel>
+    <Stepper activeStep={props.activeStep} alternativeLabel style={{ marginBottom: '1rem' }}>
       {['Login', 'Shipping Address', 'Payment Method', 'Place Order'].map((step) => (
         <Step key={step}>
           <StepLabel>{step}</StepLabel>
--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -3,12 +3,17 @@ import type { NextPage } from 'next';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormHelperText from '@mui/material/FormHelperText';
+import Radio from '@mui/material/Radio';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
 import Cookies from 'js-cookie';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import Layout from '../components/Layout';
 import Link from '../components/Link';
@@ -33,7 +38,7 @@ const PaymentPage: NextPage = () => {
   // https://stackoverflow.com/questions/71275687/type-of-handlesubmit-parameter-in-react-hook-form
   const {
     handleSubmit,
-    control,
+    register,
     formState: { errors },
   } = useForm<IFFormData>();
 
@@ -49,8 +54,10 @@ const PaymentPage: NextPage = () => {
     //   payload: { fullName, address, city, postalCode, country } as IFPayment,
     // });
     // router.push('/payment');
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const xxxxx = paymentMode;
+    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const xxxxx = paymentMode;
+    // eslint-disable-next-line no-console
+    console.log('paymentMode', paymentMode);
   };
 
   const hasMount = useRef(false);
@@ -113,24 +120,28 @@ const PaymentPage: NextPage = () => {
           <StyledForm className={`${PREFIX}-form`} onSubmit={handleSubmit(submitHandler)}>
             <List>
               <ListItem>
-                visa
-                <Controller
-                  name="paymentMode"
-                  control={control}
-                  defaultValue=""
-                  rules={{ required: true }}
-                  render={({ field }): JSX.Element => (
-                    <TextField
-                      variant="outlined"
-                      fullWidth
-                      id="paymentMode"
-                      label="Payment Mode"
-                      error={Boolean(errors.paymentMode)}
-                      helperText={errors.paymentMode ? 'Payment Mode is required' : ''}
-                      {...field}
+                {/* https://codeat21.com/material-ui-with-react-hook-form-validation-with-error-messages/ */}
+                <FormControl error={Boolean(errors.paymentMode)}>
+                  <FormLabel component="legend">Payment Method</FormLabel>
+                  <RadioGroup aria-label="paymentMode" name="paymentMode" /* row */>
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio {...register('paymentMode', { required: 'Payment Method is required' })} />}
+                      label="Female"
                     />
-                  )}
-                />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio {...register('paymentMode', { required: 'Payment Method is required' })} />}
+                      label="Male"
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={<Radio {...register('paymentMode', { required: 'Payment Method is required' })} />}
+                      label="Other"
+                    />
+                  </RadioGroup>
+                  <FormHelperText style={{ color: '#d32f2f' }}>{errors.paymentMode?.message}</FormHelperText>
+                </FormControl>
               </ListItem>
               <ListItem>
                 <Button variant="contained" color="primary" type="submit" fullWidth>
@@ -139,7 +150,7 @@ const PaymentPage: NextPage = () => {
               </ListItem>
               <ListItem>
                 <Link href="/shipping" style={{ margin: 'auto', fontSize: '1rem' }}>
-                  Back to Shipping Address
+                  Back to Shipping
                 </Link>
               </ListItem>
             </List>
