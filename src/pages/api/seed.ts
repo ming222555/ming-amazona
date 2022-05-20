0e22eab2ff821abdcbf1442633da239b7f3dbf8d
ming222555 <ming222555@yahoo.com>
2022-05-20 14:00:04 +0800

    At seed.ts * call dropCollection() on 'orders' table

--- src/pages/api/seed.ts
+++ src/pages/api/seed.ts
@@ -6,6 +6,7 @@ import Order from '../../db/models/Order';
 import db from '../../db/db';
 import data from '../../db/seeddata';
 import { onError, onNoMatch } from '../../utils/error/backend/error';
+import mongoose from 'mongoose';
 
 // https://stackoverflow.com/questions/67009540/property-status-does-not-exist-on-type-serverresponse-ts2339
 const handler = nc<NextApiRequest, NextApiResponse>({
@@ -22,6 +23,11 @@ handler.get(async (req, res) => {
   await Order.deleteMany();
   await db.disconnect();
   const status = 201;
+  mongoose.connection.db.dropCollection('orders', function (err) {
+    // eslint-disable-next-line no-console
+    console.log('dropCollection error', err);
+  });
+
   res.status(status).send({ message: 'seeded successfully', status });
 });
 
