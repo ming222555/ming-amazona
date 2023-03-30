f653cdc9eb062913aceb623b2c244e1768eafe14
ming222555 <ming222555@yahoo.com>
2023-03-31 00:03:27 +0800

    Portal to show more on a product. Done cf css

--- src/components/shared/dialogs/DialogInfo.tsx
+++ src/components/shared/dialogs/DialogInfo.tsx
@@ -6,7 +6,7 @@ import styles from './DialogInfo.module.scss';
 export default function DialogInfo(props: { onCancel: () => void; info: string }): JSX.Element {
   return (
     <Dialog onCancel={props.onCancel}>
-      <div className={styles.DialogBody}>
+      {/* <div className={styles.DialogBody}>
         <p className={styles.info}>{props.info}</p>
         <div className={styles.Actions + ' p-3'}>
           <button
@@ -17,6 +17,38 @@ export default function DialogInfo(props: { onCancel: () => void; info: string }
             Ok
           </button>
         </div>
+      </div> */}
+      <div className={styles.DialogInfo + ' ' + styles.categories}>
+        <nav className={styles.category__tabs}>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext + ' ' + styles.active}>Local</span>
+            <div className={styles.category__tabpane}>Pane local</div>
+          </a>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Goods</span>
+            <div className={styles.category__tabpane}>Pane goods</div>
+          </a>
+          {/* <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Hotels & Travel</span>
+            <div className={styles.category__tabpane}>Pane hotels & travel</div>
+          </a>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Coupons</span>
+            <div className={styles.category__tabpane}>Pane coupons</div>
+          </a>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Coupons 2</span>
+            <div className={styles.category__tabpane}>Pane coupons 2</div>
+          </a>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Coupons 3</span>
+            <div className={styles.category__tabpane}>Pane coupons 3</div>
+          </a>
+          <a className={styles.category__tab}>
+            <span className={styles.category__tabtext}>Coupons 4</span>
+            <div className={styles.category__tabpane}>Pane coupons 4</div>
+          </a> */}
+        </nav>
       </div>
     </Dialog>
   );
